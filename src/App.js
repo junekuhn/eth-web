5171c9c5a70ee53746d1829c0984789b6703c51a
justinkuhn <justintkuhn@gmail.com>
2021-04-27 11:10:56 -0400

    react-three-fiber init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh position={[0,0,-5]}>
+          <cylinderGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
