36d345bd50abf22aaf628a874dd3020dad58c99f
justinkuhn <justintkuhn@gmail.com>
2021-04-27 13:51:46 -0400

    glb support

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import "./index.css";
+import React, { useRef, useState, Suspense } from "react";
+import Model from "./Model";
+
 
 export default function App() {
+  
+  const myMesh = useRef();
+  const [active, setActive] = useState(false);
+
   return (
     <div id="canvas-container">
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh position={[0,0,-5]}>
+        <OrbitControls />
+        <ambientLight intensity={1} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <mesh scale={active ? 1.5 : 1} onClick={() => setActive(!active)} ref={myMesh}>
           <cylinderGeometry />
           <meshNormalMaterial />
         </mesh>
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+
       </Canvas>
     </div>
   );
